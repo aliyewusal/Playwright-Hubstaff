9a3044188afba3000b9ce3b633ecf37946c84046
aliyewusal <aliyewusal1@gmail.com>
2025-07-11 09:52:21 +0200

    Add Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import playwright from 'eslint-plugin-playwright';
+
+
+export default defineConfig([
+  {
+    // Ignore files and directories that don't need linting
+    ignores: [
+      "**/node_modules/**",
+      "**/playwright-report/**",
+      "**/test-results/**", 
+      "**/ctrf/**",
+      "**/playwright/**",
+      "*.config.*",
+      "package.json",
+      "README.md",
+      "**/.git/**",
+      "**/.*", // Ignore dotfiles
+    ],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      // Customize Playwright rules
+      // ...
+    },
+  },
+]);
